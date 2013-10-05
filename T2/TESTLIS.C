6af408d8a77b95d7c986a2ffd8a9521beb02edad
GuilhermeBerger <guilherme.berger@gmail.com>
2013-10-05 18:24:49 -0300

    Modifica comentarios do TESTLIS

--- T2/TESTLIS.C
+++ T2/TESTLIS.C
@@ -4,19 +4,10 @@
 *  Arquivo gerado:              TestLIS.c
 *  Letras identificadoras:      TLIS
 *
-*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
-*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\LISTA.BSW
-*
-*  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
-*  Gestor:  LES/DI/PUC-Rio
-*  Autores: avs
-*
-*  $HA Histórico de evolução:
-*     Versão  Autor    Data     Observações
-*     4       avs   01/fev/2006 criar linguagem script simbólica
-*     3       avs   08/dez/2004 uniformização dos exemplos
-*     2       avs   07/jul/2003 unificação de todos os módulos em um só projeto
-*     1       avs   16/abr/2003 início desenvolvimento
+*  Projeto: T2 INF1301 2013.2
+*  Autores: gb - Guilherme Berger
+*           fl - Felipe Luiz
+*           jv - João Vicente
 *
 ***************************************************************************/
 
