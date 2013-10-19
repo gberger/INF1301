6314c0b742fdd05284756ab83126cfa6edc63b8a
gberger42 <guilherme.berger@gmail.com>
2013-10-19 02:55:47 -0300

    Script de distribuicao: dist.bat

---
+++ T2/dist.bat
@@ -0,0 +1,44 @@
+mkdir ..\dist
+mkdir ..\dist\TRAB2-1
+mkdir ..\dist\TRAB2-2
+mkdir ..\dist\TRAB2-3
+
+
+copy LEIAME.txt ..\dist\ /Y
+copy "RELATORIO-Felipe Luiz.txt" ..\dist\ /Y
+copy "RELATORIO-Guilherme Berger.txt" ..\dist\ /Y
+copy "RELATORIO-Joao Vicente.txt" ..\dist\ /Y
+copy TestaTudo.bat ..\dist\ /Y
+
+copy GENERICO.H ..\dist\TRAB2-1\ /Y
+copy LERPARM.H ..\dist\TRAB2-1\ /Y
+copy LISTA.C ..\dist\TRAB2-1\ /Y
+copy LISTA.H ..\dist\TRAB2-1\ /Y
+copy TestaLista.bat ..\dist\TRAB2-1\ /Y
+copy TesteLista.script ..\dist\TRAB2-1\ /Y
+copy TESTLIS.C ..\dist\TRAB2-1\ /Y
+copy TST_ESPC.H ..\dist\TRAB2-1\ /Y
+
+copy GENERICO.H ..\dist\TRAB2-2\ /Y
+copy LERPARM.H ..\dist\TRAB2-2\ /Y
+copy LISTA.C ..\dist\TRAB2-2\ /Y
+copy LISTA.H ..\dist\TRAB2-2\ /Y
+copy TestaVertice.bat ..\dist\TRAB2-2\ /Y
+copy TesteVertice.script ..\dist\TRAB2-2\ /Y
+copy TESTVER.C ..\dist\TRAB2-2\ /Y
+copy TST_ESPC.H ..\dist\TRAB2-2\ /Y
+copy VERTICE.C ..\dist\TRAB2-2\ /Y
+copy VERTICE.H ..\dist\TRAB2-2\ /Y
+
+copy GENERICO.H ..\dist\TRAB2-3\ /Y
+copy GRAFO.C ..\dist\TRAB2-3\ /Y
+copy GRAFO.H ..\dist\TRAB2-3\ /Y
+copy LERPARM.H ..\dist\TRAB2-3\ /Y
+copy LISTA.C ..\dist\TRAB2-3\ /Y
+copy LISTA.H ..\dist\TRAB2-3\ /Y
+copy TestaGrafo.bat ..\dist\TRAB2-3\ /Y
+copy TesteGrafo.script ..\dist\TRAB2-3\ /Y
+copy TESTGRA.C ..\dist\TRAB2-3\ /Y
+copy TST_ESPC.H ..\dist\TRAB2-3\ /Y
+copy VERTICE.C ..\dist\TRAB2-3\ /Y
+copy VERTICE.H ..\dist\TRAB2-3\ /Y
