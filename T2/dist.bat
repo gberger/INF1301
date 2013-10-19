4eb84b01ee416d733d7ea2ec4f5b70bc72be1e8f
gberger42 <guilherme.berger@gmail.com>
2013-10-19 03:10:13 -0300

    Atualiza script de distribuicao

--- T2/dist.bat
+++ T2/dist.bat
@@ -1,46 +1,44 @@
-mkdir ..\dist
-mkdir ..\dist\TRAB2-1
-mkdir ..\dist\TRAB2-2
-mkdir ..\dist\TRAB2-3
+mkdir ..\dist-T2
+mkdir ..\dist-T2\TRAB2-1
+mkdir ..\dist-T2\TRAB2-2
+mkdir ..\dist-T2\TRAB2-3
 
-copy LEIAME.txt ..\dist\ /Y
-copy "RELATORIO-Felipe Luiz.txt" ..\dist\ /Y
-copy "RELATORIO-Guilherme Berger.txt" ..\dist\ /Y
-copy "RELATORIO-Joao Vicente.txt" ..\dist\ /Y
-copy TestaTudo.bat ..\dist\ /Y
+copy LEIAME.txt          ..\dist-T2\ /Y
+copy "RELATORIO-*.txt"   ..\dist-T2\ /Y
+copy TestaTudo.bat       ..\dist-T2\ /Y
 
-copy ArcaboucoTeste.lib ..\dist\TRAB2-1 /Y
-copy GENERICO.H ..\dist\TRAB2-1\ /Y
-copy LERPARM.H ..\dist\TRAB2-1\ /Y
-copy LISTA.C ..\dist\TRAB2-1\ /Y
-copy LISTA.H ..\dist\TRAB2-1\ /Y
-copy TestaLista.bat ..\dist\TRAB2-1\ /Y
-copy TesteLista.script ..\dist\TRAB2-1\ /Y
-copy TESTLIS.C ..\dist\TRAB2-1\ /Y
-copy TST_ESPC.H ..\dist\TRAB2-1\ /Y
+copy ArcaboucoTeste.lib  ..\dist-T2\TRAB2-1\ /Y
+copy GENERICO.H          ..\dist-T2\TRAB2-1\ /Y
+copy LERPARM.H           ..\dist-T2\TRAB2-1\ /Y
+copy LISTA.C             ..\dist-T2\TRAB2-1\ /Y
+copy LISTA.H             ..\dist-T2\TRAB2-1\ /Y
+copy TestaLista.bat      ..\dist-T2\TRAB2-1\ /Y
+copy TesteLista.script   ..\dist-T2\TRAB2-1\ /Y
+copy TESTLIS.C           ..\dist-T2\TRAB2-1\ /Y
+copy TST_ESPC.H          ..\dist-T2\TRAB2-1\ /Y
 
-copy ArcaboucoTeste.lib ..\dist\TRAB2-2 /Y
-copy GENERICO.H ..\dist\TRAB2-2\ /Y
-copy LERPARM.H ..\dist\TRAB2-2\ /Y
-copy LISTA.C ..\dist\TRAB2-2\ /Y
-copy LISTA.H ..\dist\TRAB2-2\ /Y
-copy TestaVertice.bat ..\dist\TRAB2-2\ /Y
-copy TesteVertice.script ..\dist\TRAB2-2\ /Y
-copy TESTVER.C ..\dist\TRAB2-2\ /Y
-copy TST_ESPC.H ..\dist\TRAB2-2\ /Y
-copy VERTICE.C ..\dist\TRAB2-2\ /Y
-copy VERTICE.H ..\dist\TRAB2-2\ /Y
+copy ArcaboucoTeste.lib  ..\dist-T2\TRAB2-2\ /Y
+copy GENERICO.H          ..\dist-T2\TRAB2-2\ /Y
+copy LERPARM.H           ..\dist-T2\TRAB2-2\ /Y
+copy LISTA.C             ..\dist-T2\TRAB2-2\ /Y
+copy LISTA.H             ..\dist-T2\TRAB2-2\ /Y
+copy TestaVertice.bat    ..\dist-T2\TRAB2-2\ /Y
+copy TesteVertice.script ..\dist-T2\TRAB2-2\ /Y
+copy TESTVER.C           ..\dist-T2\TRAB2-2\ /Y
+copy TST_ESPC.H          ..\dist-T2\TRAB2-2\ /Y
+copy VERTICE.C           ..\dist-T2\TRAB2-2\ /Y
+copy VERTICE.H           ..\dist-T2\TRAB2-2\ /Y
 
-copy ArcaboucoTeste.lib ..\dist\TRAB2-3 /Y
-copy GENERICO.H ..\dist\TRAB2-3\ /Y
-copy GRAFO.C ..\dist\TRAB2-3\ /Y
-copy GRAFO.H ..\dist\TRAB2-3\ /Y
-copy LERPARM.H ..\dist\TRAB2-3\ /Y
-copy LISTA.C ..\dist\TRAB2-3\ /Y
-copy LISTA.H ..\dist\TRAB2-3\ /Y
-copy TestaGrafo.bat ..\dist\TRAB2-3\ /Y
-copy TesteGrafo.script ..\dist\TRAB2-3\ /Y
-copy TESTGRA.C ..\dist\TRAB2-3\ /Y
-copy TST_ESPC.H ..\dist\TRAB2-3\ /Y
-copy VERTICE.C ..\dist\TRAB2-3\ /Y
-copy VERTICE.H ..\dist\TRAB2-3\ /Y
+copy ArcaboucoTeste.lib  ..\dist-T2\TRAB2-3\ /Y
+copy GENERICO.H          ..\dist-T2\TRAB2-3\ /Y
+copy GRAFO.C             ..\dist-T2\TRAB2-3\ /Y
+copy GRAFO.H             ..\dist-T2\TRAB2-3\ /Y
+copy LERPARM.H           ..\dist-T2\TRAB2-3\ /Y
+copy LISTA.C             ..\dist-T2\TRAB2-3\ /Y
+copy LISTA.H             ..\dist-T2\TRAB2-3\ /Y
+copy TestaGrafo.bat      ..\dist-T2\TRAB2-3\ /Y
+copy TesteGrafo.script   ..\dist-T2\TRAB2-3\ /Y
+copy TESTGRA.C           ..\dist-T2\TRAB2-3\ /Y
+copy TST_ESPC.H          ..\dist-T2\TRAB2-3\ /Y
+copy VERTICE.C           ..\dist-T2\TRAB2-3\ /Y
+copy VERTICE.H           ..\dist-T2\TRAB2-3\ /Y
