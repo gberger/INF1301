b957e82bd583a76327ff869e7325fee680c98f77
Guilherme Berger <guilherme.berger@gmail.com>
2013-11-10 21:03:27 -0200

    Adiciona módulo de implementação de peça

--- T3/PECA.C
+++ T3/PECA.C
@@ -1,5 +1,5 @@
 /***************************************************************************
-*  $MCI Módulo de implementação: Módulo peca
+*  $MCI Módulo de implementação: Módulo peça
 *
 *  Arquivo gerado:              PECA.C
 *  Letras identificadoras:      PEC
@@ -12,48 +12,99 @@
 ***************************************************************************/
 
 #include <malloc.h>
-#include <string.h>
 
 #define PECA_OWN
 #include "PECA.H"
 #undef PECA_OWN
 
+#include "CLASSE_PECA.H"
+
 /***********************************************************************
 *
-*  $TC Tipo de dados: GRA Descritor da célula do grafo
+*  $TC Tipo de dados: GRA Descritor peça
 *
 *  $ED Descrição do tipo
-*     Descreve a organização da célula
+*     Descreve a organização da peça
 *
 ***********************************************************************/
 
-   typedef struct PEC_tagPeca {
+  typedef struct PEC_tagPeca {
 	   CPC_tppClassePeca pClassePeca;
 	   PEC_tpJogador jogador;
 	} PEC_tpPeca;
 
-   typedef PEC_tpPeca * PEC_tppPeca;
-
-
 /*****  Código das funções exportadas pelo módulo  *****/
 
 /***********************************************************************
-*
-*  $FC Função: GRA Criar grafo
-*
+*  $FC Função: PEC Criar peça
 ***********************************************************************/
 
-     PEC_tpCondRet PEC_CriarPeca( PEC_tppPeca * ppPeca , CPC_tppClassePeca pClassePeca , PEC_tpJogador jogador ) {
+    PEC_tpCondRet PEC_CriarPeca( PEC_tppPeca * ppPeca , CPC_tppClassePeca pClassePeca , PEC_tpJogador jogador ) {
+      PEC_tpPeca * pPeca;
 
-     	ppPeca = malloc(sizeof(PEC_tpPeca));
+      if(pClassePeca == NULL){
+        return PEC_CondRetPonteiroNulo;
+      }
 
-     	if (ppPeca = NULL) {
-     		return PEC_CondRetFaltouMemoria; 
-     	}
+      pPeca = (PEC_tpPeca*)malloc(sizeof(PEC_tpPeca));
 
+      if (ppPeca = NULL) {
+        return PEC_CondRetFaltouMemoria; 
+      }
+
+      pPeca->pClassePeca = pClassePeca;
+      pPeca->jogador = jogador;
+
+      *ppPeca = pPeca;
      	
+      return PEC_CondRetOk;
+    }
 
-     }
+
+
+/***********************************************************************
+*  $FC Função: PEC Destruir peça
+***********************************************************************/
+
+   PEC_tpCondRet PEC_DestruirPeca( PEC_tppPeca pPeca ) {
+      if(pPeca == NULL){
+        return PEC_CondRetPonteiroNulo;
+      }
+
+      free(pPeca);
+
+      return PEC_CondRetOk;
+   }
+
+
+/***********************************************************************
+*  $FC Função: PEC Obter classe da peça
+***********************************************************************/
+
+   PEC_tpCondRet PEC_ObterClassePeca( PEC_tppPeca pPeca , CPC_tppClassePeca * ppClassePeca ) {
+      if(pPeca == NULL){
+        return PEC_CondRetPonteiroNulo;
+      }
+
+      *ppClassePeca = pPeca->pClassePeca;
+
+      return PEC_CondRetOk;
+   }
+
+
+/***********************************************************************
+*  $FC Função: PEC Obter jogador de peça
+***********************************************************************/
+
+   PEC_tpCondRet PEC_ObterJogador( PEC_tppPeca pPeca , PEC_tpJogador * pJogador ) {
+      if(pPeca == NULL){
+        return PEC_CondRetPonteiroNulo;
+      }
+
+      *pJogador = pPeca->jogador;
+
+      return PEC_CondRetOk;
+   }
 
 
 /********** Fim do módulo de implementação: Módulo grafo dirigido **********/
