8aa1806b68d08f2e9a651dbb43a94df75fc3c239
Felipe Luiz <ferocha@globo.com>
2013-12-01 05:08:02 -0200

    Comeca batch

---
+++ T4/TestaTudo.bat
@@ -0,0 +1,15 @@
+rem testa todos os scripts
+
+cls
+
+if exist estatisticas.estat  del estatisticas.estat
+
+pmod4        /sTesteGrafoInstrum             /lTesteGrafoInstrum              /aestatisticas
+
+
+TRAB4-2      /sTesteGrafo             /lTesteGrafo    /aestatisticas
+
+
+:sai
+
+pause
